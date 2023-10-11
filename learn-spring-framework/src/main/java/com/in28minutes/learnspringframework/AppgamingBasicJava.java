81f91972cd47f176a261865e227bf83518a544fe
VitorLima <vitorgl@ciandt.com>
2023-10-10 21:16:30 -0300

    first commit

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppgamingBasicJava.java
@@ -0,0 +1,12 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+
+public class AppgamingBasicJava {
+    public static void main(String[] args) {
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+    }
+}
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnspringframework.game;
+
+public class GameRunner {
+    MarioGame game;
+
+    public GameRunner(MarioGame game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Running game: " + game);
+    }
+}
